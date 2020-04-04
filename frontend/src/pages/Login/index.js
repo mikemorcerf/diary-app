ddb2c7b94a8ec957e59432e0ecac17f22823e6f0
Michael Moura <mikemorcerf@gmail.com>
2020-04-03 21:26:17 -0400

    design login page

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FiCornerRightUp } from 'react-icons/fi'
 
 import './styles.css';
 
@@ -9,20 +10,22 @@ export default function Login () {
   return (
     <div className="login-container">
       <section className="form">
-        <img src={logoImg} alt="Diary App logo" />
+        <img src={logoImg} className="logo-size" alt="Diary App logo" />
+        <form>
+          <h1>Hello there. Log in here:</h1>
+
+          <input placeholder="Your email" />
+          <input placeholder="password" />
+          <button className="button" type="submit">Open diary</button>
+
+          <a href="/register">
+            <FiCornerRightUp size={16} className="pinkColor" />
+            Don't have an account? Sign up
+          </a>
+        </form>
       </section>
 
-      <form>
-        <h1>Hello, log in here:</h1>
-
-        <input placeholder="Your email" />
-        <input placeholder="password" />
-        <button type="submit">Open diary</button>
-
-        <a href="/register">Don't have an account? Sign up :)</a>
-      </form>
-
-      <img src={welcomeImg} alt="Welcome to Diary App" />
+      <img src={welcomeImg} className="image-size" alt="Welcome to Diary App" />
     </div>
   );
 }
