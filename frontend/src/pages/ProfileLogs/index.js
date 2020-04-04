f72ad666097da0ec389932b997b5624bbb1e24d0
Michael Moura <mikemorcerf@gmail.com>
2020-04-04 16:46:08 -0400

    before installing html-react-parser

--- frontend/src/pages/ProfileLogs/index.js
+++ frontend/src/pages/ProfileLogs/index.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import Header from '../Header';
 
+import './styles.css';
+
 export default function () {
   return (
     <div>
       <Header />
       <div className="profile-logs-container">
-        <ul>
-          <li>
-            
-          </li>
-        </ul>        
+        <div className="content">
+          <h1>Diary Logs:</h1>
+          <ul>
+            <li>
+              <h2>Log date</h2>
+              <section>
+                <p>Calorie Intake:<span>value</span></p>
+                <p>Exercise Time:value</p>
+                <p>Mood:value</p>
+              </section>
+              <section>
+                <p>Vitamin Taken:value</p>
+                <p>Energy Level:value</p>
+                <p>Sleep Quality:value</p>
+              </section>
+            </li>
+          </ul>
+
+        </div>
       </div>
     </div>
   );
