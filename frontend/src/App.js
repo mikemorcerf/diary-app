baa7fc655e49844d9a3ab3728882330ffa9a4af3
Michael Moura <mikemorcerf@gmail.com>
2020-04-05 15:20:31 -0400

    design new log page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './global.css';
+
 
 import Routes from './routes';
 
--- frontend/src/pages/NewLog/index.js
+++ frontend/src/pages/NewLog/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Header from '../Header';
+import { Form } from 'react-bootstrap';
 
 import './styles.css';
 
@@ -11,21 +11,46 @@ export default function NewLog() {
       <div className="new-log-container">
         <div className="content">
         <h1>How are you doing today?</h1>
-          <form>
-            <section>
-              <p>Full name</p>
-              <p>Email address</p>
-              <p>Member since date</p>
-            </section>
-            <section>
-              <Link to="/profile/logs" className="button">
-                <span>See Diary entries</span>
-              </Link>
-              <Link to="/profile/logs/new" className="button">
-                <span>Create new entry</span>
-              </Link>
-            </section>
-          </form>
+          <Form>
+            <div className="form-content">
+              <section>
+                <span className="time">
+                  <input label="Exercise" type="number" placeholder="Hours exercising" min="0" max="23" />
+                  <input type="number" placeholder="Minutes exercising" min="0" max="59" />
+                </span>
+                <input type="number" placeholder="Calory Intake" min="0.1" max="35000.0" />
+                <div className="pick-attribute">
+                  Vitamin Taken? <Form.Check type="switch" className="vitamin-switch-class" id="vitamin-switch" label="" />
+                </div>
+              </section>
+              <section>
+                <div className="pick-attribute">
+                  <Form.Group controlId="energy-range" className="range-class" >
+                    <Form.Label style={{minWidth:'160px', marginRight:'15px', top:'100px'}} >Energy level:</Form.Label>
+                    <Form.Control style={{paddingBottom:'14px', border:'none'}} type="range" custom min={1} max={5} />
+                  </Form.Group>
+                </div>
+                <div className="pick-attribute">
+                  <Form.Group controlId="sleep-range" className="range-class" >
+                    <Form.Label style={{minWidth:'160px', marginRight:'15px', top:'100px'}} >Sleep quality:</Form.Label>
+                    <Form.Control style={{paddingBottom:'14px', border:'none'}} type="range" custom min={1} max={5} />
+                  </Form.Group>
+                </div>
+                <div className="pick-attribute">
+                  <Form.Group controlId="mood-select" className="range-class">
+                    <Form.Label>Mood</Form.Label>
+                    <Form.Control as="select" style={{position:'relative', bottom:'5px', border:'none', maxWidth:'150px'}} custom>
+                      <option>Happy 😄</option>
+                      <option>Calm 😊</option>
+                      <option>Sad 😔</option>
+                      <option>Annoyed 😤</option>
+                    </Form.Control>
+                  </Form.Group>
+                </div>
+              </section>
+            </div>
+            <button className="button" type="submit" >Save</button>
+          </Form>
         </div>
       </div>
     </div>
